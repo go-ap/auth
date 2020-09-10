47d05959d08ff583099b3af017fa15870d5141bb
mariusor <marius@federated.id>
2020-09-10 19:27:51 +0200

    Adding the placeholders for badger storage

---
+++ badger.go
@@ -0,0 +1,118 @@
+package auth
+
+import (
+	"github.com/dgraph-io/badger"
+	"github.com/go-ap/errors"
+	"github.com/openshift/osin"
+	"path"
+	"sync"
+)
+
+type badgerStorage struct {
+	d       *badger.DB
+	m       sync.Mutex
+	path    string
+	logFn   loggerFn
+	errFn   loggerFn
+}
+
+type BadgerConfig struct {
+	Path  string
+	LogFn loggerFn
+	ErrFn loggerFn
+}
+
+// NewBadgerStore returns a new badger storage instance.
+func NewBadgerStore(c FSConfig) *badgerStorage {
+	fullPath := path.Join(path.Clean(c.Path), folder)
+	if err := mkDirIfNotExists(fullPath); err != nil {
+		return nil
+	}
+	b := badgerStorage{
+		path:    fullPath,
+		m:       sync.Mutex{},
+		logFn:   emptyLogFn,
+		errFn:   emptyLogFn,
+	}
+	if c.ErrFn != nil {
+		b.errFn = c.ErrFn
+	}
+	if c.LogFn != nil {
+		b.logFn = c.LogFn
+	}
+	return &b
+}
+
+// Open opens the badger database if possible.
+func (s *badgerStorage) Open() error {
+	var err error
+	s.m.Lock()
+	c := badger.DefaultOptions(s.path).WithLogger(logger{
+		logFn: s.logFn,
+		errFn: s.errFn,
+	})
+	s.d, err = badger.Open(c)
+	if err != nil {
+		err = errors.Annotatef(err, "unable to open storage")
+	}
+	return err
+}
+
+// Close closes the badger database if possible.
+func (s *badgerStorage) Close() {
+	if s.d == nil {
+		return
+	}
+	s.d.Close()
+	s.m.Unlock()
+}
+
+// Clone
+func (s *badgerStorage) Clone() osin.Storage {
+	return s
+}
+
+// GetClient
+func (s *badgerStorage) GetClient(id string) (osin.Client, error) {
+	return nil, nil
+}
+
+// SaveAuthorize
+func (s *badgerStorage) SaveAuthorize(data *osin.AuthorizeData) error {
+	return nil
+}
+
+// LoadAuthorize
+func (s *badgerStorage) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
+	return nil, nil
+}
+
+// RemoveAuthorize
+func (s *badgerStorage) RemoveAuthorize(code string) error {
+	return nil
+}
+
+// SaveAccess
+func (s *badgerStorage) SaveAccess(data *osin.AccessData) error {
+	return nil
+}
+
+// LoadAccess
+func (s *badgerStorage) LoadAccess(token string) (*osin.AccessData, error) {
+	return nil, nil
+}
+
+// RemoveAccess
+func (s *badgerStorage) RemoveAccess(token string) error {
+	return nil
+}
+
+// LoadRefresh
+func (s *badgerStorage) LoadRefresh(token string) (*osin.AccessData, error) {
+	return nil, nil
+}
+
+// RemoveRefresh revokes or deletes refresh AccessData.
+func (s *badgerStorage) RemoveRefresh(token string) error {
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,12 @@ go 1.13
 require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/dgraph-io/badger v1.6.1
+	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
 	github.com/go-ap/activitypub v0.0.0-20200830140334-e0b519daad28
-	github.com/go-ap/client v0.0.0-20200909115600-ad8ab62d361c // indirect
+	github.com/go-ap/client v0.0.0-20200909115600-ad8ab62d361c
 	github.com/go-ap/errors v0.0.0-20200702155720-f662512ba418
-	github.com/go-ap/storage v0.0.0-20200830140640-73cfa1d1477b // indirect
+	github.com/go-ap/storage v0.0.0-20200830140640-73cfa1d1477b
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/google/uuid v1.1.1 // indirect
