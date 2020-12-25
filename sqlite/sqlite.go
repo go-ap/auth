5c130105bcbfaa31503acbb8e877abd66512f8b3
Marius Orcsik <marius@habarnam.ro>
2020-12-25 15:35:28 +0100

    Added constants with the required queries

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -76,42 +76,42 @@ type Config struct {
 
 var errNotImplemented = errors.NotImplementedf("not implemented")
 
-const createClientTable = `create table client(
-	id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-	secret TEXT NOT NULL,
-	redirect_uri TEXT NOT NULL,
-	extra
+const createClientTable = `create table "client"(
+	"id" INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	"secret" TEXT NOT NULL,
+	"redirect_uri" TEXT NOT NULL,
+	"extra"
 );`
 
-const createAuthorizeTable = `CREATE TABLE authorize (
-	id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-	client_id INTEGER REFERENCES client(id),
-	code TEXT NOT NULL,
-	expires_in INTEGER,
-	scope,
-	redirect_uri TEXT NOT NULL,
-	state,
-	created_at DEFAULT CURRENT_TIMESTAMP,
-	extra
+const createAuthorizeTable = `CREATE TABLE "authorize" (
+	"id" INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	"client_id" INTEGER REFERENCES client(id),
+	"code" TEXT NOT NULL,
+	"expires_in" INTEGER,
+	"scope",
+	"redirect_uri" TEXT NOT NULL,
+	"state",
+	"created_at" DEFAULT CURRENT_TIMESTAMP,
+	"extra"
 );`
 
-const createAccessTable = `CREATE TABLE access (
-	id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-	client_id INTEGER REFERENCES client(id),
-	authorize_id INTEGER REFERENCES authorize(id),
-	previous TEXT NOT NULL,
-	access_token TEXT NOT NULL,
-	refresh_token TEXT NOT NULL,
-	expires_in INTEGER,
-	scope,
-	redirect_uri TEXT NOT NULL,
-	created_at DEFAULT CURRENT_TIMESTAMP,
-	extra
+const createAccessTable = `CREATE TABLE "access" (
+	"id" INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	"client_id" INTEGER REFERENCES client(id),
+	"authorize_id" INTEGER REFERENCES authorize(id),
+	"previous" TEXT NOT NULL,
+	"access_token" TEXT NOT NULL,
+	"refresh_token" TEXT NOT NULL,
+	"expires_in" INTEGER,
+	"scope",
+	"redirect_uri" TEXT NOT NULL,
+	"created_at" DEFAULT CURRENT_TIMESTAMP,
+	"extra"
 );`
-const createRefreshTable = `CREATE TABLE refresh (
-	id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-	access TEXT NOT NULL REFERENCES access(access_token),
-	token TEXT NOT NULL
+const createRefreshTable = `CREATE TABLE "refresh" (
+	"id" INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+	"access" TEXT NOT NULL REFERENCES access(access_token),
+	"token" TEXT NOT NULL
 );`
 
 func Bootstrap(db *sql.DB, cl osin.Client) error {
@@ -153,17 +153,19 @@ func (s *stor) Open() error {
 	return nil
 }
 
+const getClients = "SELECT id, secret, redirect_uri, extra FROM client;"
+
 // ListClients
 func (s *stor) ListClients() ([]osin.Client, error) {
 	return nil, errNotImplemented
 }
 
-const selectClient = "SELECT id, secret, redirect_uri, extra FROM client WHERE id=?"
+const getClient = "SELECT id, secret, redirect_uri, extra FROM client WHERE id=?;"
 
 // GetClient
 func (s *stor) GetClient(id string) (osin.Client, error) {
 	var c osin.DefaultClient
-	rows, err := s.conn.Query(selectClient, id)
+	rows, err := s.conn.Query(getClient, id)
 	if err == sql.ErrNoRows {
 		return nil, errors.NewNotFound(err, "")
 	} else if err != nil {
@@ -180,57 +182,84 @@ func (s *stor) GetClient(id string) (osin.Client, error) {
 	return &c, err
 }
 
+const updateClient = "UPDATE client SET (secret, redirect_uri, extra) = (?2, ?3, ?4) WHERE id=?1"
+
 // UpdateClient
 func (s *stor) UpdateClient(c osin.Client) error {
 	return errNotImplemented
 }
+
+const createClient = "INSERT INTO client (id, secret, redirect_uri, extra) VALUES (?0, ?1, ?2, ?3)"
 
 // CreateClient
 func (s *stor) CreateClient(c osin.Client) error {
 	return errNotImplemented
 }
 
+const removeClient = "DELETE FROM client WHERE id=?"
+
 // RemoveClient
 func (s *stor) RemoveClient(id string) error {
 	return errNotImplemented
 }
+
+const saveAuthorize = `INSERT INTO authorize (client, code, expires_in, scope, redirect_uri, state, created_at, extra) 
+	VALUES (?0, ?1, ?2, ?3, ?4, ?5, ?6, ?7)`
 
 // SaveAuthorize saves authorize data.
 func (s *stor) SaveAuthorize(data *osin.AuthorizeData) error {
 	return errNotImplemented
 }
 
+const loadAuthorize = "SELECT client, code, expires_in, scope, redirect_uri, state, created_at, extra FROM authorize WHERE code=? LIMIT 1"
+
 // LoadAuthorize looks up AuthorizeData by a code.
 func (s *stor) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
 	return nil, errNotImplemented
 }
+
+const removeAuthorize = "DELETE FROM authorize WHERE code=?"
 
 // RemoveAuthorize revokes or deletes the authorization code.
 func (s *stor) RemoveAuthorize(code string) error {
 	return errNotImplemented
 }
 
+const saveAccess = `INSERT INTO access (client, authorize, previous, access_token, refresh_token, expires_in, scope, redirect_uri, created_at, extra) 
+	VALUES (?0, ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)`
+
 // SaveAccess writes AccessData.
 func (s *stor) SaveAccess(data *osin.AccessData) error {
 	return errNotImplemented
 }
+
+const loadAccess = `SELECT client, authorize, previous, access_token, refresh_token, expires_in, scope, redirect_uri, created_at, extra 
+	FROM access WHERE access_token=? LIMIT 1`
 
 // LoadAccess retrieves access data by token. Client information MUST be loaded together.
 func (s *stor) LoadAccess(code string) (*osin.AccessData, error) {
 	return nil, errNotImplemented
 }
 
+const removeAccess = "DELETE FROM access WHERE access_token=?"
+
 // RemoveAccess revokes or deletes an AccessData.
 func (s *stor) RemoveAccess(code string) error {
 	return errNotImplemented
 }
+
+const loadRefresh = "SELECT access FROM refresh WHERE token=? LIMIT 1"
 
 // LoadRefresh retrieves refresh AccessData. Client information MUST be loaded together.
 func (s *stor) LoadRefresh(code string) (*osin.AccessData, error) {
 	return nil, errNotImplemented
 }
 
+const removeRefresh = "DELETE FROM refresh WHERE token=?"
+
 // RemoveRefresh revokes or deletes refresh AccessData.
 func (s *stor) RemoveRefresh(code string) error {
 	return errNotImplemented
 }
+
+const saveRefresh = "INSERT INTO refresh (token, access) VALUES (?0, ?1)"
