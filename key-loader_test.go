32729c6fa361b128956a14f0f9c9919295cb0bce
Marius Orcsik <marius@federated.id>
2026-05-02 14:41:41 +0200

    Confirming issue with tags.pub (#473) is not due to being unable to load actor, key for http-signatures verification

--- key-loader_test.go
+++ key-loader_test.go
@@ -141,7 +141,169 @@ func Test_keyLoader_loadRemoteKey(t *testing.T) {
 			}),
 			wantErr: errors.Gonef("key does not exist"),
 		},
+		{
+			name: "tags.pub actor issue #473",
+			arg:  "http://example.com/user/activitypub",
+			handlerFn: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				status := http.StatusOK
+				payload := `{
+  "@context": [
+    "https://www.w3.org/ns/activitystreams",
+    "https://w3id.org/security/v1",
+    "https://purl.archive.org/socialweb/webfinger",
+    "https://purl.archive.org/miscellany"
+  ],
+  "id": "http://example.com/user/activitypub",
+  "type": "Service",
+  "inbox": "http://example.com/user/activitypub/inbox",
+  "webfinger": "activitypub@tags.pub",
+  "publicKey": {
+    "id": "http://example.com/user/activitypub/publickey",
+    "type": "CryptographicKey",
+    "owner": "http://example.com/user/activitypub",
+    "publicKeyPem": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n",
+    "to": "as:Public"
+  },
+  "endpoints": {
+    "sharedInbox": "http://example.com/shared/inbox"
+  },
+  "followers": "http://example.com/user/activitypub/followers",
+  "following": "http://example.com/user/activitypub/following",
+  "icon": {
+    "type": "Link",
+    "href": "http://example.com/user/activitypub/icon"
+  },
+  "liked": "http://example.com/user/activitypub/liked",
+  "manuallyApprovesFollowers": false,
+  "name": "#activitypub",
+  "outbox": "http://example.com/user/activitypub/outbox",
+  "preferredUsername": "activitypub",
+  "summary": "<p>Follow me if you&apos;re interested in the #activitypub hashtag.</p>",
+  "to": "as:Public",
+  "url": {
+    "type": "Link",
+    "href": "http://example.com/profile/activitypub",
+    "mediaType": "text/html"
+  }
+}`
+				w.Header().Set("Content-Type", "application/activity+json")
+				w.Header().Set("Cache-Control", "public")
+				w.WriteHeader(status)
+				_, _ = w.Write([]byte(payload))
+			}),
+			want: vocab.Actor{
+				ID:        "http://example.com/user/activitypub",
+				Type:      vocab.ServiceType,
+				Name:      vocab.DefaultNaturalLanguage("#activitypub"),
+				Icon:      &vocab.Link{Type: vocab.LinkType, Href: "http://example.com/user/activitypub/icon"},
+				Summary:   vocab.DefaultNaturalLanguage("<p>Follow me if you&apos;re interested in the #activitypub hashtag.</p>"),
+				URL:       &vocab.Link{Type: vocab.LinkType, MediaType: "text/html", Href: "http://example.com/profile/activitypub"},
+				To:        vocab.ItemCollection{vocab.IRI("as:Public")},
+				Inbox:     vocab.IRI("http://example.com/user/activitypub/inbox"),
+				Outbox:    vocab.IRI("http://example.com/user/activitypub/outbox"),
+				Following: vocab.IRI("http://example.com/user/activitypub/following"),
+				Followers: vocab.IRI("http://example.com/user/activitypub/followers"),
+				Liked:     vocab.IRI("http://example.com/user/activitypub/liked"),
+				Endpoints: &vocab.Endpoints{SharedInbox: vocab.IRI("http://example.com/shared/inbox")},
+				PublicKey: vocab.PublicKey{
+					ID:           "http://example.com/user/activitypub/publickey",
+					Owner:        "http://example.com/user/activitypub",
+					PublicKeyPem: `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n`,
+				},
+			},
+			wantKey: &vocab.PublicKey{
+				ID:           "http://example.com/user/activitypub/publickey",
+				Owner:        "http://example.com/user/activitypub",
+				PublicKeyPem: "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n",
+			},
+		},
+		{
+			name: "tags.pub key issue #473",
+			arg:  "http://example.com/user/activitypub/publickey",
+			handlerFn: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				status := http.StatusOK
+				payload := `{
+  "id": "http://example.com/user/activitypub/publickey",
+  "type": "CryptographicKey",
+  "owner": "http://example.com/user/activitypub",
+  "publicKeyPem": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n",
+  "to": "as:Public"
+}`
+				w.Header().Set("Content-Type", "application/activity+json")
+				w.Header().Set("Cache-Control", "public")
+				w.WriteHeader(status)
+				if !strings.HasSuffix(r.URL.Path, "/publickey") {
+					payload = `{
+  "@context": [
+    "https://www.w3.org/ns/activitystreams",
+    "https://w3id.org/security/v1",
+    "https://purl.archive.org/socialweb/webfinger",
+    "https://purl.archive.org/miscellany"
+  ],
+  "id": "http://example.com/user/activitypub",
+  "type": "Service",
+  "inbox": "http://example.com/user/activitypub/inbox",
+  "webfinger": "activitypub@tags.pub",
+  "publicKey": {
+    "id": "http://example.com/user/activitypub/publickey",
+    "type": "CryptographicKey",
+    "owner": "http://example.com/user/activitypub",
+    "publicKeyPem": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n",
+    "to": "as:Public"
+  },
+  "endpoints": {
+    "sharedInbox": "http://example.com/shared/inbox"
+  },
+  "followers": "http://example.com/user/activitypub/followers",
+  "following": "http://example.com/user/activitypub/following",
+  "icon": {
+    "type": "Link",
+    "href": "http://example.com/user/activitypub/icon"
+  },
+  "liked": "http://example.com/user/activitypub/liked",
+  "manuallyApprovesFollowers": false,
+  "name": "#activitypub",
+  "outbox": "http://example.com/user/activitypub/outbox",
+  "preferredUsername": "activitypub",
+  "summary": "<p>Follow me if you&apos;re interested in the #activitypub hashtag.</p>",
+  "to": "as:Public",
+  "url": {
+    "type": "Link",
+    "href": "http://example.com/profile/activitypub",
+    "mediaType": "text/html"
+  }
+}`
+				}
+				_, _ = w.Write([]byte(payload))
+			}),
+			want: vocab.Actor{
+				ID:        "http://example.com/user/activitypub",
+				Type:      vocab.ServiceType,
+				Name:      vocab.DefaultNaturalLanguage("#activitypub"),
+				Icon:      &vocab.Link{Type: vocab.LinkType, Href: "http://example.com/user/activitypub/icon"},
+				Summary:   vocab.DefaultNaturalLanguage("<p>Follow me if you&apos;re interested in the #activitypub hashtag.</p>"),
+				URL:       &vocab.Link{Type: vocab.LinkType, MediaType: "text/html", Href: "http://example.com/profile/activitypub"},
+				To:        vocab.ItemCollection{vocab.IRI("as:Public")},
+				Inbox:     vocab.IRI("http://example.com/user/activitypub/inbox"),
+				Outbox:    vocab.IRI("http://example.com/user/activitypub/outbox"),
+				Following: vocab.IRI("http://example.com/user/activitypub/following"),
+				Followers: vocab.IRI("http://example.com/user/activitypub/followers"),
+				Liked:     vocab.IRI("http://example.com/user/activitypub/liked"),
+				Endpoints: &vocab.Endpoints{SharedInbox: vocab.IRI("http://example.com/shared/inbox")},
+				PublicKey: vocab.PublicKey{
+					ID:           "http://example.com/user/activitypub/publickey",
+					Owner:        "http://example.com/user/activitypub",
+					PublicKeyPem: `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n`,
+				},
+			},
+			wantKey: &vocab.PublicKey{
+				ID:           "http://example.com/user/activitypub/publickey",
+				Owner:        "http://example.com/user/activitypub",
+				PublicKeyPem: "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4U5c7F2cpNTKoOz4Pp+g\nThvIMxux3mVtnT58uZv94kzzdL8s+b6ldpuxIN7x+7RmfovWyeKsaot2nM8NWaTZ\nbAVJyCdL1IJ+tTBmeTvtPCKXUuP3xG9qJSFqUPyxEymdVrpdG1uOON6s7p8BVz94\nrMCoKIzUO90pcAV9GTayh2ilTEGHyZW8WEwIZOv9JruPDMBfCiq8LTIDTHGkTXzg\nrU3xfmUjYEt2+McKlEJsh5NL6cZyoPRdI1Ci0whO6UNSNolJxRfuw7Y5EIt6xpqI\nWHkRWAOovE9f4yNAvVNG1SP+6nwjK9tTvAP670QP4RsSS5x/wxcN6mQE1yWjkX9e\ncwIDAQAB\n-----END PUBLIC KEY-----\n",
+			},
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			srv := httptest.NewServer(tt.handlerFn)
@@ -157,8 +319,8 @@ func Test_keyLoader_loadRemoteKey(t *testing.T) {
 			if !cmp.Equal(act, tt.want, EquateItems) {
 				t.Errorf("GetKey() got actor = %s", cmp.Diff(tt.want, act, EquateItems))
 			}
-			if !cmp.Equal(key, tt.wantKey, EquatePublicKeys) {
-				t.Errorf("GetKey() got key = %s", cmp.Diff(tt.wantKey, key, EquatePublicKeys))
+			if !cmp.Equal(key, tt.wantKey) {
+				t.Errorf("GetKey() got key = %s", cmp.Diff(tt.wantKey, key))
 			}
 		})
 	}
